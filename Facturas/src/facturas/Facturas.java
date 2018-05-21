ed3c22728b82e2240753385675c8701e1d1dc6d1
brianrivera1604 <rivera.brian99@gmail.com>
2018-05-20 20:15:05 -0600

    Update Facturas.java
    Updated credits

--- Facturas/src/facturas/Facturas.java
+++ Facturas/src/facturas/Facturas.java
@@ -12,7 +12,7 @@ import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 /**
  *
- * @author Brian
+ * @author Brian Rivera y Rudy Sanchez
  */
 public class Facturas {
 
